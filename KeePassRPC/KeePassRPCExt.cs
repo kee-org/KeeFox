13373c8c0a80d849a64644d5d35472f859a2dd44
Chris Tomlinson <github@musites.com>
2012-10-07 13:47:02 +0100

    1.1.3a1

--- KeePassRPC/KeePassRPCExt.cs
+++ KeePassRPC/KeePassRPCExt.cs
@@ -58,7 +58,7 @@ namespace KeePassRPC
         //private static LifetimeServices fakeHack = new LifetimeServices();
 
         // version information
-        public static readonly Version PluginVersion = new Version(1,1,1);
+        public static readonly Version PluginVersion = new Version(1,1,3);
                 
         private KeePassRPCServer _RPCServer;
         private KeePassRPCService _RPCService;
