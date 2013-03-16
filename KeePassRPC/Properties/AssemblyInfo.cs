ed3015d5e86f506a7594428e686f6d006cd020e0
Chris Tomlinson <github@musites.com>
2013-03-16 12:55:16 +0000

    More locale tweaks and prep for beta

--- KeePassRPC/Properties/AssemblyInfo.cs
+++ KeePassRPC/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Chris Tomlinson")]
 [assembly: AssemblyProduct("KeePass Plugin")]
-[assembly: AssemblyCopyright("Copyright © 2012 Chris Tomlinson")]
+[assembly: AssemblyCopyright("Copyright © 2013 Chris Tomlinson")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -41,4 +41,4 @@ using System.Runtime.InteropServices;
 
 // Assembly version information
 [assembly: AssemblyVersion("2.0.15.*")]
-[assembly: AssemblyFileVersion("1.1.8.0")] // also change PluginVersion in KeePassRPCExt.cs!
+[assembly: AssemblyFileVersion("1.2.0.0")] // also change PluginVersion in KeePassRPCExt.cs!
