5520b09cfc7f0f3e2fa2ad2279f3bfb8598d3e01
Chris Tomlinson <github@musites.com>
2013-03-16 12:27:29 +0000

    updated locales and prep for beta release

--- Firefox addon/KeeFox/modules/json.js
+++ Firefox addon/KeeFox/modules/json.js
@@ -45,7 +45,7 @@ function jsonrpcClient() {
     this.tokenCurlyCount = 0;
     this.tokenSquareCount = 0;
     this.adjacentBackslashCount = 0;
-    this.clientVersion = [1,1,8];
+    this.clientVersion = [1,2,0];
 }
 
 jsonrpcClient.prototype = new session();
