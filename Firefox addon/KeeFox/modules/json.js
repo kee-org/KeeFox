892a2a0ffe35c64a7f38ca6256c9ee3492a2ee07
Chris Tomlinson <github@musites.com>
2013-03-25 19:16:00 +0000

    1.2.2b1

--- Firefox addon/KeeFox/modules/json.js
+++ Firefox addon/KeeFox/modules/json.js
@@ -45,7 +45,7 @@ function jsonrpcClient() {
     this.tokenCurlyCount = 0;
     this.tokenSquareCount = 0;
     this.adjacentBackslashCount = 0;
-    this.clientVersion = [1,2,1];
+    this.clientVersion = [1,2,2];
 }
 
 jsonrpcClient.prototype = new session();
--- KeePassRPC/Properties/AssemblyInfo.cs
+++ KeePassRPC/Properties/AssemblyInfo.cs
@@ -41,4 +41,4 @@ using System.Runtime.InteropServices;
 
 // Assembly version information
 [assembly: AssemblyVersion("2.0.15.*")]
-[assembly: AssemblyFileVersion("1.2.1.0")] // also change PluginVersion in KeePassRPCExt.cs!
+[assembly: AssemblyFileVersion("1.2.2.0")] // also change PluginVersion in KeePassRPCExt.cs!
