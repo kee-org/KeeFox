c8a09988409746599ae91d8a4028dd0939159691
Chris Tomlinson <github@musites.com>
2013-08-15 23:53:30 +0100

    1.2.5b1; Danish, French, Korean, Portuguese and Russian languages created / updated (some not complete yet)

--- Firefox addon/KeeFox/modules/json.js
+++ Firefox addon/KeeFox/modules/json.js
@@ -45,7 +45,7 @@ function jsonrpcClient() {
     this.tokenCurlyCount = 0;
     this.tokenSquareCount = 0;
     this.adjacentBackslashCount = 0;
-    this.clientVersion = [1,2,4];
+    this.clientVersion = [1,2,5];
 }
 
 jsonrpcClient.prototype = new session();
