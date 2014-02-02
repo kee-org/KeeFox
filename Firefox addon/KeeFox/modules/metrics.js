c8bed844d7d34a616e12cdc1b50315f41a009dac
Chris Tomlinson <github@musites.com>
2014-02-02 17:45:43 +0000

    Metrics manager no longer collects screen size data

--- Firefox addon/KeeFox/modules/metrics.js
+++ Firefox addon/KeeFox/modules/metrics.js
@@ -83,20 +83,6 @@ function mm () {
         this.ii.browserType = Services.appinfo.name;
         this.ii.browserVersion = Services.appinfo.version;
 
-        let wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
-                     .getService(Ci.nsIWindowMediator);
-        let mainWindow = wm.getMostRecentWindow("navigator:browser") ||
-            wm.getMostRecentWindow("mail:3pane");
-
-        // Screen and window size
-        this.ii.screenWidth = mainWindow.screen.width;
-        this.ii.screenHeight = mainWindow.screen.height;
-
-        //TODO: These values are usually incorrect (presumably because the module
-        // initialises before or during main window init but I've not verified that)
-        this.ii.windowWidth = mainWindow.top.outerWidth;
-        this.ii.windowHeight = mainWindow.top.outerHeight;
-
         // OS type
         this.ii.OSType = Services.appinfo.OS;
 
@@ -353,11 +339,7 @@ function mm () {
                 "OSType": mm.ii.OSType,
                 "OSversion": mm.ii.OSversion,
                 "locale": mm.ii.locale,
-                "addonVersion": mm.ii.addonVersion,
-                "screenWidth": mm.ii.screenWidth,
-                "screenHeight": mm.ii.screenHeight,
-                "windowWidth": mm.ii.windowWidth,
-                "windowHeight": mm.ii.windowHeight
+                "addonVersion": mm.ii.addonVersion
             };
             mm.set("message",JSON.stringify(msg),function () { cb(); });
         });
