4bcbfe971cae1d2a4344ff978131506836e5e34f
luckyrat <luckyrat@musites.com>
2014-04-23 19:21:09 +0100

    setup instruction modification for new UI; 1.4.0b1

--- KeePassRPC/Properties/AssemblyInfo.cs
+++ KeePassRPC/Properties/AssemblyInfo.cs
@@ -41,4 +41,4 @@ using System.Runtime.InteropServices;
 
 // Assembly version information
 [assembly: AssemblyVersion("2.0.15.*")]
-[assembly: AssemblyFileVersion("1.3.5.0")] // also change PluginVersion in KeePassRPCExt.cs!
+[assembly: AssemblyFileVersion("1.4.0.0")] // also change PluginVersion in KeePassRPCExt.cs!
