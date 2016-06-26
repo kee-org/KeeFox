bbcd4ccc4727949408c15d2b2eb365273cff7e8a
luckyrat <luckyrat@musites.com>
2016-06-26 13:10:31 +0100

    Include domain identification fixes from upstream library

--- Firefox addon/KeeFox/modules/kprpcClient.js
+++ Firefox addon/KeeFox/modules/kprpcClient.js
@@ -55,7 +55,7 @@ function kprpcClient() {
     this.requestId = 1;
     this.callbacks = {};
     this.callbacksData = {};
-    this.clientVersion = [1,6,0];
+    this.clientVersion = [1,6,2];
     this.authPromptAborted = false;
     
     // We manually create HMACs to protect the integrity of our AES encrypted messages
